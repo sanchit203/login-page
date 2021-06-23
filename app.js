19fbd9256370268889481d18fd0a25e984ef4b8e
sanchit203 <sanchitmunjal203@gmail.com>
2021-06-23 23:16:39 +0530

    Hashing added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology : true})
 const app = express()
 const port = 3000
@@ -17,9 +17,6 @@ const userSchema = new mongoose.Schema({
     email : String,
     password : String
 });
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +37,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
@@ -54,7 +51,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email : username}, function(err, foundUser){
         if(err){
